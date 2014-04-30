449d59c3fd7c8917e98151e9618141f963c76328
Vic Ramon <vic@vicramon.com>
2014-04-30 19:40:10 -0400

    Create leads model, controller, and serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :leads
+    end
+  end
+
   root to: 'home#index'
   get '*path', to: 'home#index'
 end
